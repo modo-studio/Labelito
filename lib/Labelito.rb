a74655ecea04ef269f48f879c4535cf7b3e74e0d
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-09-14 08:22:09 +0200

    Connect the Thor client with the tool

--- lib/Labelito.rb
+++ lib/Labelito.rb
@@ -1,25 +1,28 @@
 require_relative "Labelito/version"
 require_relative "Labelito/template_reader"
 require_relative "Labelito/errors"
+require_relative "Labelito/tool"
 require "thor"
 
-# module Labelito
-#
-#   # http://whatisthor.com/
-#   class CommandTool < Thor
-#
-#     desc "clone FROM_OWNER/FROM_REPO TO_OWNER/TO_REPO", "clone the labels from an existing repository"
-#     def clone(from, to)
-#
-#     end
-#
-#     desc "list [SEARCH]", "list all of the available apps, limited by SEARCH"
-#     def list(search="")
-#       # list everything
-#     end
-#
-#   end
-#
-# end
-#
-# Labelito::CommandTool.start(ARGV)
+module Labelito
+
+  # http://whatisthor.com/
+  class CommandTool < Thor
+
+    desc "clone FROM_OWNER/FROM_REPO TO_OWNER/TO_REPO", "clone the labels from an existing repository"
+    def clone(from_repo, to_repo)
+      tool = Labelito::Tool.with_token(token)
+      tool.clone(from_repo, to_repo)
+    end
+
+    desc "create TEMPLATE_PATH REPO_OWNER/REPO", "create labels on a repository based on a template"
+    def create(template_path, repo)
+      tool = Labelito::Tool.with_token(token)
+      tool.create(template_path, repo)
+    end
+
+  end
+
+end
+
+Labelito::CommandTool.start(ARGV)
