0673764a371e057126d0565471a3b1a90fbce629
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-09-14 08:21:31 +0200

    Add getters and setters to Label and update the static constructor to return nil if there are properties missing.

--- lib/Labelito/label.rb
+++ lib/Labelito/label.rb
@@ -1,13 +1,15 @@
 module Labelito
   class Label
 
+    attr_reader :name, :color
+
     def initialize(name, color)
       @name = name
       @color = color
     end
 
     def self.from_github_label(label)
-      Label.new label[:name], label[:color]
+      return Label.new label[:name], label[:color] if label[:name] && label[:color]
     end
 
   end
